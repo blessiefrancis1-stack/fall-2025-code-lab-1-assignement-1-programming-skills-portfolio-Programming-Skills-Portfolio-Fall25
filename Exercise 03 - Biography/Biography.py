59a8c3947f8e6a3efdc384abaeac7bcf39717062
blessiefrancis1-stack <blessiefrancis1@gmail.com>
2025-11-22 19:32:51 +0400

    Create Biography.py

---
+++ Exercise 03 - Biography/Biography.py
@@ -0,0 +1,22 @@
+name=input("Enter your full name: ")
+hometown=input("Enter your hometown: ")
+
+#Safely convert age to an integer
+while True:
+    age_input=input("Enter your age: ")
+    if age_input.isdigit():
+        age=int(age_input)
+        break
+    else:
+        print("Please enter a valid age.")
+
+bio={
+    "name": name,
+    "hometown": hometown,
+    "age": age
+}
+
+print(bio["name"], bio["hometown"], bio["age"], sep="\n")
+
+
+
